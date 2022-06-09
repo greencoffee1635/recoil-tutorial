ca47694b558c0224eb65ba7ef549de5de7f18c60
greencoffee1635 <166354@naver.com>
2022-06-10 04:42:41 +0900

    Create components
    Create CharacterCount, CharacterCounter, TextInput, atoms, selectors components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import React from "react";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
-
-// Atom
-const textState = atom({
-  key: "textState", // unique ID (with respect to other atoms/selectors)
-  default: "", // default value (aka initial value)
-});
-
-// Selector
-const charCountState = selector({
-  key: "charCountState", // unique ID (with respect to other atoms/selectors)
-  get: ({ get }) => {
-    const text = get(textState);
-
-    return text.length;
-  },
-});
+import { RecoilRoot } from "recoil";
+import CharacterCounter from "./CharacterCounter";
 
 function App() {
   return (
@@ -29,37 +8,6 @@ function App() {
       <CharacterCounter />
     </RecoilRoot>
   );
-}
-
-function CharacterCounter() {
-  return (
-    <div>
-      <TextInput />
-      <CharacterCount />
-    </div>
-  );
-}
-
-function TextInput() {
-  const [text, setText] = useRecoilState(textState);
-
-  const onChange = event => {
-    setText(event.target.value);
-  };
-
-  return (
-    <div>
-      <input type='text' value={text} onChange={onChange} />
-      <br />
-      Echo: {text}
-    </div>
-  );
-}
-
-function CharacterCount() {
-  const count = useRecoilValue(charCountState);
-
-  return <>Character Count: {count}</>;
 }
 
 export default App;
